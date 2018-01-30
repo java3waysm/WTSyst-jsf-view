f7f84d7f3a763b455c5d370fc99e542fa32893d6
2163452 <2163452@REI89127.ifg.br>
2018-01-30 13:59:04 -0200

    INICIO DO PROJETO

---
+++ src/main/java/br/com/wtsyst/view/configuration/ApplicationLoaderListener.java
@@ -0,0 +1,15 @@
+package br.com.wtsyst.view.configuration;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationLoaderListener implements ApplicationListener<ContextRefreshedEvent> {
+
+
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+
+	}
+
+}
