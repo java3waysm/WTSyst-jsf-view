2113dd67ae0a84c2e0ed3f2eca30edfd85ddda95
thiago <thiago@thiago.thiago>
2018-03-23 21:15:57 -0300

    teste

--- src/main/java/br/com/wtsyst/view/configuration/ApplicationLoaderListener.java
+++ src/main/java/br/com/wtsyst/view/configuration/ApplicationLoaderListener.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 public class ApplicationLoaderListener implements ApplicationListener<ContextRefreshedEvent> {
 
 
+	
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 
 	}
